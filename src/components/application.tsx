bc7c03a9e0a0edfb0f09e99b391615f7f9d16403
Stanislav Romanov <kaermorchen@gmail.com>
2023-08-07 11:09:14 +0300

    Add react-json-view

--- src/components/application.tsx
+++ src/components/application.tsx
@@ -1,10 +1,29 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from 'react-router-dom';
+
+const parsers = [
+  {
+    id: 'php',
+    name: 'PHP',
+  },
+  {
+    id: 'twig',
+    name: 'Twig',
+  },
+];
 
 function Application() {
   return (
     <div className="flex flex-col h-screen">
       <header className="flex-none flex items-center px-3 py-4 sm:px-4 lg:px-6 shadow">
         <h1 className="flex-none text-xl font-bold">Tree-sitter explorer</h1>
+
+        <nav className="ml-4 flex items-baseline gap-4" role="navigation">
+          {parsers.map((parser) => (
+            <Link to={`parser/${parser.id}`} key={parser.id}>
+              {parser.name}
+            </Link>
+          ))}
+        </nav>
       </header>
 
       <main className="grow flex">
--- src/components/parser.tsx
+++ src/components/parser.tsx
@@ -1,11 +1,131 @@
-import { Outlet } from "react-router-dom";
+import { useState } from 'react';
+import ReactJson from 'react-json-view'
+
+const json = {
+  type: 'Template',
+  body: [
+    {
+      type: 'SetStatement',
+      declarations: [
+        {
+          type: 'VariableDeclaration',
+          name: {
+            type: 'Identifier',
+            name: 'greeting',
+          },
+          init: {
+            type: 'StringLiteral',
+            value: 'Hello World',
+          },
+        },
+      ],
+    },
+    {
+      type: 'Text',
+      value: '\n\n',
+    },
+    {
+      type: 'VariableStatement',
+      value: {
+        type: 'FilterExpression',
+        expression: {
+          type: 'Identifier',
+          name: 'greeting',
+        },
+        filter: {
+          type: 'Identifier',
+          name: 'lower',
+        },
+      },
+    },
+    {
+      type: 'Text',
+      value: '\n\n',
+    },
+    {
+      type: 'ForInStatement',
+      body: [
+        {
+          type: 'Text',
+          value: '\n  ',
+        },
+        {
+          type: 'VariableStatement',
+          value: {
+            type: 'Identifier',
+            name: 'i',
+          },
+        },
+        {
+          type: 'Text',
+          value: ',\n',
+        },
+      ],
+      variables: [
+        {
+          type: 'Identifier',
+          name: 'i',
+        },
+      ],
+      expression: {
+        type: 'CallExpression',
+        callee: {
+          type: 'Identifier',
+          name: 'range',
+        },
+        arguments: [
+          {
+            type: 'NamedArgument',
+            key: {
+              type: 'Identifier',
+              name: 'low',
+            },
+            value: {
+              type: 'NumericLiteral',
+              value: 1,
+            },
+          },
+          {
+            type: 'NamedArgument',
+            key: {
+              type: 'Identifier',
+              name: 'high',
+            },
+            value: {
+              type: 'NumericLiteral',
+              value: 10,
+            },
+          },
+          {
+            type: 'NamedArgument',
+            key: {
+              type: 'Identifier',
+              name: 'step',
+            },
+            value: {
+              type: 'NumericLiteral',
+              value: 2,
+            },
+          },
+        ],
+      },
+      alternate: null,
+    },
+  ],
+};
 
 function Parser() {
+  const [code, setCode] = useState(`hello`);
+
   return (
     <div className="flex-1 flex items-stretch mt-4">
-      <div className="flex-1 border-r-1">asd</div>
+      <textarea
+        className="flex-1 border-r-1"
+        value={code}
+        onChange={(e) => setCode(e.currentTarget.value)}
+      />
       <div className="flex-1">
-        <Outlet />
+        <ReactJson  src={json} />
       </div>
     </div>
   );
