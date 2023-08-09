2880ed49e5c79143876481ea92b3d5e2d5c2c502
Stanislav Romanov <kaermorchen@gmail.com>
2023-08-09 12:14:34 +0300

    Add select for navigation

--- src/components/application.tsx
+++ src/components/application.tsx
@@ -1,19 +1,28 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet, useNavigate, useParams } from 'react-router-dom';
 import { parsers } from '../parsers';
 
 function Application() {
+  const navigate = useNavigate();
+  const params = useParams();
+
   return (
     <div className="flex flex-col h-screen">
       <header className="flex-none flex items-center px-3 py-4 sm:px-4 lg:px-6 shadow">
         <h1 className="flex-none text-xl font-bold">Tree-sitter explorer</h1>
 
-        <nav className="ml-4 flex items-baseline gap-4" role="navigation">
-          {parsers.map((parser) => (
-            <Link to={`parser/${parser.id}`} key={parser.id}>
-              {parser.name}
-            </Link>
+        <select
+          className="block w-full rounded-md border-0 ring-1 ring-inset ring-gray-300 py-1.5 px-2 ml-4 max-w-xs bg-white"
+          onChange={(e) => {
+            navigate(`/parser/${e.target.value}`);
+          }}
+          value={params.parserId}
+        >
+          {parsers.map((item) => (
+            <option value={item.id} key={item.id}>
+              {item.name}
+            </option>
           ))}
-        </nav>
+        </select>
       </header>
 
       <main className="grow flex">
