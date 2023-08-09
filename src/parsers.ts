6270b69fd6045034f420938e2660747fa5f80d76
Stanislav Romanov <kaermorchen@gmail.com>
2023-08-09 11:18:30 +0300

    Add wasm building

--- src/parsers.ts
+++ src/parsers.ts
@@ -1,22 +1,40 @@
+import packageJson from '../package.json';
+
 export interface IParser {
   id: string;
   name: string;
   wasmUrl: string;
   initCode: string;
+  version: string;
 }
 
 export const parsers: IParser[] = [
   {
-    id: 'javascript',
+    id: 'tree-sitter-javascript',
     name: 'JavaScript',
-    wasmUrl:
-      'https://unpkg.com/tree-sitter-javascript/tree-sitter-javascript.wasm',
+    wasmUrl: '/parsers/tree-sitter-javascript.wasm',
+    version: packageJson.dependencies['tree-sitter-javascript'],
     initCode: ['2 + 4;'].join('\n'),
   },
   {
-    id: 'twig',
+    id: 'tree-sitter-twig',
     name: 'Twig',
-    wasmUrl: 'https://unpkg.com/tree-sitter-twig/tree-sitter-twig.wasm',
+    wasmUrl: 'https://unpkg.com/tree-sitter-twig@0.4.0/tree-sitter-twig.wasm',
+    version: '0.4.0',
     initCode: ['{{ var }}'].join('\n'),
+  },
+  {
+    id: 'tree-sitter-php',
+    name: 'PHP',
+    wasmUrl: '/parsers/tree-sitter-php.wasm',
+    version: packageJson.dependencies['tree-sitter-php'],
+    initCode: ['hello'].join('\n'),
+  },
+  {
+    id: 'tree-sitter-scala',
+    name: 'Scala',
+    wasmUrl: '/parsers/tree-sitter-scala.wasm',
+    version: packageJson.dependencies['tree-sitter-scala'],
+    initCode: ['hello'].join('\n'),
   },
 ];
