a29f96982b89a6b8b8b948a48163e837472a3b6a
Stanislav Romanov <kaermorchen@gmail.com>
2023-08-11 10:50:37 +0300

    Styled header menu

--- src/components/application.tsx
+++ src/components/application.tsx
@@ -2,6 +2,8 @@ import { Outlet, useNavigate, useParams } from 'react-router-dom';
 import { IParser, parsers } from '../parsers';
 import { defaultRoute, getParserById } from '..';
 import { useEffect, useState } from 'react';
+import OpenInNewIcon from 'mdi-react/OpenInNewIcon';
+import GithubIcon from 'mdi-react/GithubIcon';
 
 function Application() {
   const navigate = useNavigate();
@@ -28,7 +30,7 @@ function Application() {
         </h1>
 
         <select
-          className="block w-full rounded-md border-0 ring-1 ring-inset ring-gray-300 py-1.5 px-2 ml-4 max-w-xs bg-white"
+          className="block rounded-md border-0 ring-1 ring-inset ring-gray-300 py-1.5 px-2 ml-4 bg-white"
           onChange={(e) => {
             navigate(`/parser/${e.target.value}`);
           }}
@@ -36,21 +38,30 @@ function Application() {
         >
           {parsers.map((item) => (
             <option value={item.id} key={item.id}>
-              {item.name}
+              {item.id}
             </option>
           ))}
         </select>
 
         {parser && (
-          <a href={parser.homepage} className="ml-4">
-            {parser.id}@{parser.version}
-          </a>
+          <>
+            <a
+              href={parser.homepage}
+              className="ml-4 inline-flex items-center"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Homepage <OpenInNewIcon className="-mt-2" size={10} />
+            </a>
+            <span className="ml-4">Version: {parser.version}</span>
+          </>
         )}
 
         <a
           href="https://github.com/kaermorchen/tree-sitter-explorer"
-          className="ml-auto"
+          className="ml-auto inline-flex items-center"
         >
+          <GithubIcon className="mr-1" size={20}/>
           GitHub
         </a>
       </header>
