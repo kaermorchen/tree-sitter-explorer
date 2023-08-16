627762235cb62cfa810390d62c3d284a830ac76c
Stanislav Romanov <kaermorchen@gmail.com>
2023-08-16 17:25:59 +0300

    Add mobx reactivity to TreeNode

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import './style.css';
 import Application from './components/application';
 import Parser from './components/parser';
 import { IParser, parsers } from './parsers';
+import { observer } from 'mobx-react';
 
 export const defaultRoute = 'parser/tree-sitter-javascript';
 
@@ -42,6 +43,8 @@ const router = createBrowserRouter(
   }
 );
 
+const Main = observer(() => <RouterProvider router={router} />);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <RouterProvider router={router} />
+  <Main />
 );
